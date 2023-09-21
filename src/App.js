a0a7d56438513d0efd429d9fab952be4e3f4290b
Mélanie Fauchard <melaniefauchard0@gmail.com>
2023-09-21 12:17:14 +0200

    Création des pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import FicheLogement from './pages/FicheLogement';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Home/index.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/FicheLogement" element={<FicheLogement />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/NotFound" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
